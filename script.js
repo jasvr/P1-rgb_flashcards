9a00fc264b8622ad4a7c2a55b4e4547dcc21a7b0
Jasmin Vargas <jas.vrgs@gmail.com>
2019-03-31 16:47:06 -0400

    keep track of how many user guessed correctly out of 10 ques

--- script.js
+++ script.js
@@ -6,17 +6,33 @@ var question3 = document.rgbtest.question3.value;
 
 var correct = 0;
 
-if (question1 == "yesm"){
+if (question1 == "(255,0,0)"){
   correct++;
 }
-if (question2 == "yesn"){
+if (question2 == "(0,128,0)"){
   correct++;
 }
-if (question3 == "yeso"){
+if (question3 == "blue"){
   correct++;
+}
+//Final Score Message -adj to 10 question
+var messages = ["Great job!", "Not bad :)", "Just need some more practice :)"];
+var range;
+
+if (correct <= 3 ){
+  range =2;
+}
+
+if (correct > 0 && correct <= 10){
+  range = 1;
+}
+
+if (correct >= 10) {
+  range = 0;
 }
 
   document.getElementById("after_submit").style.visibility ="visible";
+  document.getElementById("message").innerHTML = messages[range];
   document.getElementById("final_score").innerHTML = "You got " + correct + " correct.";
 }
 
