a37b3924d4ff9811977869e5e1b75c355b8481ab
Jasmin Vargas <jas.vrgs@gmail.com>
2019-03-31 10:27:56 -0400

    add css to folders and rename pages

--- script.js
+++ script.js
@@ -4,6 +4,19 @@ $('.jscard').justFlipIt({
 }); 
 
 
+const StarWars = ['Han', 'C3PO', 'R2D2', 'Luke', 'Leia', 'Anakin'];
+
+for (i = 0; i < StarWars.length; i++){
+
+  console.log(i + "." + StarWars[i]);
+}
 
 
+questions = ['What is the red in RGB?', 'What is green in RGB?', 'What is blue in RGB?']
+
+set state 
+
+display that's correct!
+
+display You'll get it! 
 
